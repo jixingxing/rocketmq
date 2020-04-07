78e496cdda2346f3c731b24d0af59148c8c0b868
jixingxing <jixingxing@tuniu.com>
2020-04-07 12:21:22 +0800

    添加注释

--- store/src/main/java/org/apache/rocketmq/store/ha/HAConnection.java
+++ store/src/main/java/org/apache/rocketmq/store/ha/HAConnection.java
@@ -28,6 +28,9 @@ import org.apache.rocketmq.logging.InternalLoggerFactory;
 import org.apache.rocketmq.remoting.common.RemotingUtil;
 import org.apache.rocketmq.store.SelectMappedBufferResult;
 
+/**
+ * Master-Slave数据同步
+ */
 public class HAConnection {
     private static final InternalLogger log = InternalLoggerFactory.getLogger(LoggerName.STORE_LOGGER_NAME);
     private final HAService haService;
